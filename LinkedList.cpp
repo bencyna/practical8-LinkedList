f522f2d6793dc24d5e9fb4d0af28afddc6b8952c
Ben Cyna <benjamincyna@gmail.com>
2022-05-09 11:55:36 +0930

    added functions to linked list cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -5,7 +5,53 @@ LinkedList::LinkedList()
 
 }
 
+LinkedList::LinkedList(int list[100], int size)
+{
+
+}
+
+
 LinkedList::~LinkedList()
 {
+
+}
+
+void LinkedList::addFront(int newItem) {
+
+}
+
+void LinkedList::addEnd(int newItem) {
+
+}
+
+void LinkedList::addAtPosition(int position, int newItem) {
+
+}
+
+int LinkedList::search(int item) {
+
+}
+
+void LinkedList::deleteFront() {
+
+}
+
+void LinkedList::deleteEnd() {
+
+}
+
+void LinkedList::deletePosition(int position) {
+
+}
+
+int LinkedList::getItem(int position) {
+
+}
+
+void LinkedList::printItems() {
+
+}
+
+LinkedList::~LinkedList() {
 
 }
