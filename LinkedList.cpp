d286cb3c07efc0e50e70be996ec0fef7c18650f8
Ben Cyna <benjamincyna@gmail.com>
2022-05-09 11:51:45 +0930

    setup linked list header

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -4,3 +4,8 @@ LinkedList::LinkedList()
 {
 
 }
+
+LinkedList::~LinkedList()
+{
+
+}
--- LinkedList.h
+++ LinkedList.h
@@ -3,10 +3,23 @@
 
 #pragma once
 
+#include <array>
+
 class LinkedList
 {
 public:
     LinkedList();
+    LinkedList(int list[100], int size);
+    void addFront(int newItem);
+    void addEnd(int newItem);
+    void addAtPosition(int position, int newItem);
+    int search(int item);
+    void deleteFront();
+    void deleteEnd();
+    void deletePosition(int position);
+    int getItem(int position);
+    void printItems();
+    ~LinkedList();
 
 private:
     int * head;
